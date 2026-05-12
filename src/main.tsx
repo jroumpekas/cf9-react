6deffec1cd137e2a00abb355f3cbe75a71448b02
Dim.Roumpekas <jimroumpi@gmail.com>
2026-05-12 19:07:39 +0300

    chore: delete unwanted code and files

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+function App() {
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    return (
+        <>
+        </>
+    )
+}
+
+export default App
+ 
